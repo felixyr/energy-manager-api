a2870f4c6d13fe5848fa31b2cc174977981a9b01
Felix Too <microfelixs@gmail.com>
2024-11-20 22:38:40 +0000

    Create the readings controller and delete default weather controller

---
+++ EnergyManager.Web/Controllers/ReadingsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnergyManager.Web.Controllers
+{
+    public class ReadingsController: Controller
+    {
+        private readonly ILogger<ReadingsController> _logger;
+
+        public ReadingsController(ILogger<ReadingsController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
