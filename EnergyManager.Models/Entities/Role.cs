ec3c3432b996d08776f22e0c8cb30603ddefa94f
Felix Too <microfelixs@gmail.com>
2024-11-20 22:41:46 +0000

    Default user and role entities

---
+++ EnergyManager.Models/Entities/Role.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EnergyManager.Models.Entities
+{
+
+    public class Role : IdentityRole
+    {        
+        /// <summary>
+        /// Initializes a new instance of <see cref="Role"/>.
+        /// </summary>
+        /// <param name="roleName">The role name.</param>       
+        public Role(string roleName) : base(roleName)
+        {
+        }
+
+        /// <summary>
+        /// Navigation property for the users in this role.
+        /// </summary>
+        public virtual ICollection<IdentityUserRole<string>> Users { get; set; }
+
+        /// <summary>
+        /// Navigation property for claims in this role.
+        /// </summary>
+        public virtual ICollection<IdentityRoleClaim<string>> Claims { get; set; }
+    }
+
+}
---
+++ EnergyManager.Models/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EnergyManager.Models.Entities
+{
+    public class User : IdentityUser
+    {
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Navigation property for the roles this user belongs to.
+        /// </summary>
+        public virtual ICollection<IdentityUserRole<string>> Roles { get; set; }
+
+        /// <summary>
+        /// Navigation property for the claims this user possesses.
+        /// </summary>
+        public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
+    }
+}
